9e11df9c65fe72c0ff18194a8edf9d9802d139c0
Jodaille <Jodaille@users.noreply.github.com>
2023-02-27 11:01:29 +0100

    Laravel Install

---
+++ app/Console/Commands/NmapParser.php
@@ -0,0 +1,69 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use DOMElement;
+use DOMDocument;
+use DOMXPath;
+
+class NmapParser extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'nmap:parse {nmapxml}';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return int
+     */
+    public function handle()
+    {
+        $path = $this->argument('nmapxml');
+        $xml = file_get_contents($path);
+        libxml_use_internal_errors(true);
+        $dom = new DOMDocument;
+        $dom->loadXML($xml);
+        libxml_use_internal_errors(false);
+        $xpath = new DOMXPath($dom);
+        $hosts = $xpath->query('//host');
+        foreach($hosts as $host) {
+            $macAddress = false;
+            $vendor = false;
+            $status  = $host->getElementsByTagName('status')->item(0);
+            $macNode = $xpath->query('.//address[contains(@addrtype, "mac")]', $host)->item(0);
+            $ip4Node = $xpath->query('.//address[contains(@addrtype, "ipv4")]', $host)->item(0);
+
+            if ($macNode) {
+                /** @var DOMElement $macNode */
+                $macAddress = $macNode->getAttribute('addr');
+                $vendor = $macNode->getAttribute('vendor');
+            }
+
+            /** @var DOMElement $ip4Node */
+            $ipv4Address = $ip4Node->getAttribute('addr');
+            $this->info("{$macAddress} {$ipv4Address} {$vendor}");
+        }
+    }
+}
